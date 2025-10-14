7bc4852132a94e54404ef933199358dc27cd7412
SagarSigroha <sagarsigroha19@gmail.com>
2025-10-14 22:36:00 +0530

    n

--- 100Practice work/Extra practice/sketch.js
+++ 100Practice work/Extra practice/sketch.js
@@ -1,22 +1,10 @@
 function setup() {
+    createCanvas(500, 500,WEBGL);
   
-  createCanvas(1000, 1000, WEBGL);
-  //WEBGL makes origin at centre of the canvas 
-    background(200, 150, 50);
+    background(200);
 }
 
 function draw() {
-rectMode(CENTER)
-
-  // -ve values becs of origin at centre and canvas is divided into -x,x,-y,y
-
-  // rotate around yaxis
-  rotateY(frameCount * 0.05);
-  fill(40, 180, 50, 150);
-  stroke("blue");
-  rect(100, -100, 200, 300, 90, 0, 90, 0);
-
-  fill(mouseY);
-
+ellipse(10, height/5,width/3,height/3 )
  
  }
