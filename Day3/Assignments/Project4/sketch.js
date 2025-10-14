ae78afeb470e1abfb3a1b5ae8f7adf9983b83452
Sagar <sagarsigroha19@gmail.com>
2025-10-14 09:41:06 +0530

    new

---
+++ Day3/Assignments/Project4/sketch.js
@@ -0,0 +1,24 @@
+let x, y, size;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  noStroke();
+  background(20);
+  x = 0;
+  y = 0;
+  size = 20;
+}
+
+function draw() {
+  size = 10 + mouseX / 10;
+  fill(random(255), random(255), random(255));
+  rect(x, y, size, size);
+  x = x + size;
+  if (x > width) {
+    x = 0;
+    y = y + size;
+  }
+  if (y > height) {
+    y = 0;
+  }
+}
